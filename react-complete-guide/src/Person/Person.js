e23e5392a8f031ad9b576b3aec8fa069297ea7b8
olympia <owojcik7@gmail.com>
2019-05-28 10:14:07 -0400

    Complete 36. Understanding the Children Property

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const person = props => {
   return (
-    <p>
-      I'm {props.name} and I'm {props.age} years old
-    </p>
+    <div>
+      <p>
+        I'm {props.name} and I'm {props.age} years old
+        <p>{props.children}</p>
+      </p>
+    <div/>
   );
 };
 
