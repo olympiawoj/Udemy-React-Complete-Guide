8a00b043ace585843bc8fb3910c9d80f295b7b2d
olympia <owojcik7@gmail.com>
2019-06-02 14:32:54 -0400

    Add input and handleChanges to assignment1 User Input component

--- assignment1-base-syntax/src/components/UserInput.js
+++ assignment1-base-syntax/src/components/UserInput.js
@@ -1,8 +1,31 @@
 import React from "react";
 
+//http://cheng.logdown.com/posts/2016/03/26/683329
+//https://medium.com/@etherealm/super-vs-super-props-in-react-class-components-58658af6ecf2
+
 class UserInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputUserName: ""
+    };
+  }
+
+  handleChanges = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
-    return <div>Test</div>;
+    return (
+      <input
+        type="text"
+        value={this.state.inputUserName}
+        name="inputUserName"
+        onChange={this.handleChanges}
+      />
+    );
   }
 }
 
--- assignment1-base-syntax/src/components/UserOutput.js
+++ assignment1-base-syntax/src/components/UserOutput.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const UserOutput = () => {
-  return <div>Test User Outuut</div>;
+  return (
+    <div>
+      <p>Para 1</p>
+      <p>Para 2</p>
+    </div>
+  );
 };
 
 export default UserOutput;
