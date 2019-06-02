fdad70081eab1e6bca9646c970a2f08ce83773c7
olympia <owojcik7@gmail.com>
2019-06-02 14:18:25 -0400

    Create UserInput and UserOutput components and test with fake JSX

---
+++ assignment1-base-syntax/src/components/UserInput.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class UserInput extends React.Component {
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default UserInput;
---
+++ assignment1-base-syntax/src/components/UserOutput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserOutput = () => {
+  return <div>Test User Outuut</div>;
+};
+
+export default UserOutput;
