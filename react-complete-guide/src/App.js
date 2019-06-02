6b2cb62d89596ae99f9cfb4ef7a9648d3ac967a9
olympia <owojcik7@gmail.com>
2019-06-02 11:40:16 -0400

    Complete 38-42 on Props & State, Handling Events with Methods, Manipulating State, etc

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,19 @@ class App extends Component {
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
       { name: "Stephanie", age: 26 }
-    ]
+    ],
+    otherState: "some other value"
+  };
+
+  switchNameHandler = () => {
+    // console.log("was clicked");
+    this.setState({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 }
+      ]
+    });
   };
 
   render() {
@@ -16,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
