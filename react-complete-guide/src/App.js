62a0d322823af38d63c92765cb7c1c4b533eec3a
olympia <owojcik7@gmail.com>
2019-06-15 15:21:25 -0400

    Complete 68. Using Radium for Media Queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium, { StyleRoot } from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -60,7 +61,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
 
     let persons = null;
@@ -80,20 +85,41 @@ class App extends Component {
           })}
         </div>
       );
+      //ovverride bg color
       style.backgroundColor = "red";
+      //override hover
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black"
+      };
     }
 
+    //turns array of string into 1 string
+    //valid CSS class list
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red"); //classes red
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push("bold"); //classes red +bold
+    }
+
+    // let classes = ["red", "bold"].join(" ");
+
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is working</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(" ")}>This is working</p>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
