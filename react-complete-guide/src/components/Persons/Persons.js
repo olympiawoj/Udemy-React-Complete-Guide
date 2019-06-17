4e332a115b3f33d88de378cdced1e04857db4cf9
olympia <owojcik7@gmail.com>
2019-06-17 10:37:08 -0400

    Complete 89. Component Update Lifecycle (for props Changes)

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const Persons = props => {
-  console.log("[Persons.js] rendering...");
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        key={person.id}
-        age={person.age}
-        click={() => props.clicked(index)}
-        name={person.name}
-        changed={event => props.changed(event, person.id)}
-      />
-    );
-  });
-};
+class Persons extends Component {
+  //   static getDerivedStateFromProps(props, state) {
+  //     console.log("[Persons.js] getDerivedStateFromProps running");
+  //     return state;
+  //   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    //have to return true or false, nothing not an option - return true if React should cont updating, false if it shouldn't
+    console.log("[Persons.js] shouldComponentUpdate running");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshotBeforeUpdate running");
+    return { message: "Snapshot!" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate....");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          key={person.id}
+          age={person.age}
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
 export default Persons;
