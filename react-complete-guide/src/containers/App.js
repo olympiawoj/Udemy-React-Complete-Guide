15b7909640cdf000cf9ccabe7b45702aeb57723f
olympia <owojcik7@gmail.com>
2019-07-08 21:11:52 -0400

    Complete 104- Another Form of HOCs

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
-import WithClass from "../hoc/WithClass";
+import withClass from "../hoc/withClass";
+import Aux from "../hoc/Aux";
 
 class App extends Component {
   constructor(props) {
@@ -91,7 +92,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -110,9 +111,9 @@ class App extends Component {
         ) : null}
 
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
