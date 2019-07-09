72ced43f080f392a8979b786f89b930cbf200d25
olympia <owojcik7@gmail.com>
2019-07-08 20:47:44 -0400

    Complete HOCs Intro

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
+import Aux from "../../../hoc/Aux";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I'm {this.props.age} years old
         </p>
@@ -15,7 +16,7 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
     );
   }
 }
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/WithClass";
 
 class App extends Component {
   constructor(props) {
@@ -90,7 +91,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -109,7 +110,7 @@ class App extends Component {
         ) : null}
 
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
